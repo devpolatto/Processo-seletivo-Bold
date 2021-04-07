465b035f6556a48b49c933c59eaa0d619589ded6
devpolatto <polatto.xcode.job@gmail.com>
2021-04-07 16:48:19 -0300

    External JS with problem fixed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,51 @@
-// const page_one = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1";
-// const page_two = "frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2";
+const page_one = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1";
+const page_two = "frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=2";
 
-// fetch(page_one, {
-//     method: 'get'
-// }).then( (response)  => {
-//     response.json().then( (data) => {
-//         console.log(data)
-//     } )
-// })
+const productList = document.getElementById('product-list')
+const moreButton = document.getElementById('more-btn')
+
+function disableButton(){
+    const btn = document.getElementById('more-btn');
+    btn.remove()
+}
+
+function showResult(data){
+    console.log(data)
+    handleProduct(data)
+}
+
+function handleProduct(products){
+    var elementFather = document.getElementById('product-list');
+
+    for(let index of products){
+        let product = `<div id="product" class="product">
+                <img src="${index.image}" alt="">
+                <h4 id="name-product">${index.name}</h4>
+                <p>${index.description}</p>
+                <p>De: R$${index.oldPrice}</p>
+                <p>Por: R$${index.price}</p>
+                <p>ou ${index.installments.count}x de R$${index.installments.value}</p>
+                <button>Comprar</button>
+            </div>
+        `
+        elementFather.insertAdjacentHTML("beforeend", product)
+    }
+}
+
+function getProducts(url){
+    fetch(page_one, {
+        method: 'get',
+        mode: 'cors'
+    }).then( response => { response.json()
+        .then( data => { showResult(data.products) } )
+    } )
+}; getProducts(page_one)
+
+function moreProducts(url){
+    getProducts(url)
+}
+
+moreButton.addEventListener("click", () => { 
+    moreProducts(page_two) 
+    disableButton()
+});
--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ let port = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "/views/index.html")
+// })
 
 app.listen(port, () => {
     console.log("server running on port " + port)
